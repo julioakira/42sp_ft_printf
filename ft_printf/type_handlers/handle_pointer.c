ea8ab289e6b0cf843df4cf0b9d41138c318536ea
julioakira <julio.akira.pedroso@gmail.com>
2021-10-12 20:06:51 -0300

    fixes zero pointer

--- ft_printf/type_handlers/handle_pointer.c
+++ ft_printf/type_handlers/handle_pointer.c
@@ -6,7 +6,7 @@
 /*   By: jakira-p <jakira-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 00:53:58 by jakira-p          #+#    #+#             */
-/*   Updated: 2021/09/29 18:36:53 by jakira-p         ###   ########.fr       */
+/*   Updated: 2021/10/12 20:01:42 by jakira-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	handle_pointer(va_list args, t_metadata *data)
 	ptr_value = va_arg(args, unsigned long);
 	if (!ptr_value)
 	{
-		ptr = ft_strdup("(nil)");
+		ptr = ft_strdup("0x0");
 		ft_putstr_fd(ptr, 1);
 		data->char_counter += ft_strlen(ptr);
 	}
